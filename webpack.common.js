4180f7b57ff2eb26fdb041f8e67ae57f72584cd6
Justin Mirfield <justin.mirfield@gmail.com>
2022-05-04 15:18:28 -0400

    Fixed webpack configuration, separated out dev

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/template.html',
+        }),
+    ],
+};
